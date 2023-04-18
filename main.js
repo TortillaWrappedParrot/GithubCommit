baa4a7c736d78d480a8f7f9c04fd6b6e583013d6
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-04-18 12:19:05 -0700

    Finished Calculate Function
    Finished the calculate function and changed it to stop clearing on calculate

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 document.getElementById("calculate").onclick = function(){
-    let num1 = document.getElementById("num1").value
-    let operator = document.getElementById("operator").value
-    let num2 = document.getElementById("num2").value
-    if (!isNaN(num1) && !isNaN(num2) && !isNaN(operator)){
-        alert("works")
+    let num1 = document.getElementById("num1").value;
+    let operator = document.getElementById("operator").value;
+    let num2 = document.getElementById("num2").value;
+    if (!isNaN(num1) && !isNaN(num2) && isNaN(operator)){
+        if (operator == "+" || operator == "-" || operator == "*" || operator == "/" || operator == "%" || operator == "**"){
+            let test = eval(num1 + operator + num2);
+            alert(test);
+        } else {
+            alert("invalid operator")
+        }
     } else {
-        alert("Input not valid")
+        alert("Input not valid");
     }
 };
